61b57181a6c2fa0ae27e5ec4452351a88e00eb38
JackReis01 <jackreis2019@gmail.com>
2018-09-18 09:52:56 -0500

    updated text

--- Chapter-5/src/practice/ageGame.java
+++ Chapter-5/src/practice/ageGame.java
@@ -15,7 +15,7 @@ public class ageGame {
 		System.out.println("Please enter your age to be insulted.");
 		usersAge = input.nextInt();
 		
-		if (usersAge >= 12)
+		if (usersAge <= 12)
 		{
 			result = "Your a child! Go away!";
 		}
@@ -32,7 +32,47 @@ public class ageGame {
 		
 		else if (usersAge >= 26 && usersAge <= 40)
 		{
-			result = ""
+			result = "Shouldn't you be a parent by now?";
+		}
+		
+		else if (usersAge >= 41 && usersAge <= 60)
+		{
+			result = "Say hello to grandkids!";
+		}
+		
+		else if (usersAge >= 61 && usersAge <= 90)
+		{
+			result = "Say hello to GREAT grandkids!";
+		}
+		
+		else if (usersAge >= 91 && usersAge <= 100)
+		{
+			result = "Shouldn't you have died already?";
+		}
+		
+		else if (usersAge >= 101 && usersAge <= 200)
+		{
+			result = "Why are you still alive";
+		}
+		
+		else if (usersAge >= 201 && usersAge <= 300)
+		{
+			result = "Perish.";
+		}
+		
+		else if (usersAge >= 301 && usersAge <= 400)
+		{
+			result = "Your are unholy.";
+		}
+		
+		else if (usersAge >= 401 && usersAge <=1000)
+		{
+			result = "Legendary being.";
+		}
+		
+		else if (usersAge >1001)
+		{
+			result = 
 		}
 		
 	}
