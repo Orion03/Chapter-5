f7629a9101531cd529d986c724c7d6c33bb249ff
JackReis01 <jackreis2019@gmail.com>
2018-09-18 09:10:54 -0500

    updated text

---
+++ Chapter-5/src/practice/doorGame.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class doorGame {
+
+	public static void main(String[] args) {
+		
+		int doorChoice;
+		
+		System.out.println("Welcome to the amazing door choice game!");
+		System.out.println("Your host Steve Harvey");
+		System.out.println("Pick a door any door. \n1- for door One\n2- for door Two\n3- for door Three");
+	}
+
+}
