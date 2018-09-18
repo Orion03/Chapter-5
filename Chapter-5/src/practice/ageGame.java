8bb8c01f6faa988fac361520bd7fb150ebcb912a
JackReis01 <jackreis2019@gmail.com>
2018-09-18 09:38:14 -0500

    updated text

--- Chapter-5/src/practice/ageGame.java
+++ Chapter-5/src/practice/ageGame.java
@@ -1,8 +1,18 @@
 package practice;
 
+import java.util.Scanner;
+
 public class ageGame {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		
+		int usersAge;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter your age to be insulted.");
+		usersAge = input.nextInt();
 		
 		
 		
