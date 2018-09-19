e907fec992fc926a16bb39e2e3f0f4041cb230ad
JackReis01 <jackreis2019@gmail.com>
2018-09-19 09:33:08 -0500

    updated text

--- Chapter-5/src/exercises/EvenOdd.java
+++ Chapter-5/src/exercises/EvenOdd.java
@@ -1,8 +1,40 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class EvenOdd {
 
 	public static void main(String[] args) {
+		
+		int x;
+		int y;
+		
+		String result;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a number >> ");
+		x = input.nextInt();
+		
+		y = x % 2;
+		
+		if(y == 0)
+		{
+			result = "The number is even.";
+		}
+		
+		else
+		{
+			result = "The number is odd.";
+		}
+		
+		displayResult(result);
+		
+	}
+	
+	public static void displayResult (String result) {
+		
+		System.out.println(result);
 	}
 
 }
