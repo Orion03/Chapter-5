7c0926dcb0aa657b4579c802cda993bea312c1bc
JackReis01 <jackreis2019@gmail.com>
2018-09-20 09:19:21 -0500

    updated text

--- Chapter-5/src/practice/IdScanner.java
+++ Chapter-5/src/practice/IdScanner.java
@@ -16,14 +16,66 @@ public class IdScanner {
 		
 		switch(idNumber)
 		{
-		case 274904:
+		case 274894:
 			result = "John Brosius";
 			break;
 		case 316338:
 			result = "Logan DeWilfond";
 			break;
+		case 155706:
+			result = "Joseph Chalupa";
+			break;
+		case 274891:
+			result = "Jason Wells";
+		
+		case 137112:
+			result = "Adam Burton";
+			break;
+		case 137160:
+			result = "Zach Canada";
+			break;
+		case 153184:
+			result = "Jayden Graap";
+			break;
+		case 159343:
+			result = "Giovanni Jacobsen";
+			break;
+		case 129001:
+			result = "Isaac Dibbern";
+			break;
+		case 274871:
+			result = "Tavius Sterling";
+			break;
+		case 153182:
+			result = "Orion Gardner";
+			break;
+		case 274904:
+			result = "Amia Combs";
+			break;
+		case 304345:
+			result = "Cheyenne Jones";
+			break;
+		case 118314:
+			result = "Benjamen Brooks";
+			break;
+		case 146891:
+			result = "De'Antwan Hanes";
+			break;
+		case 146168:
+			result = "Noah Giles";
+			break;
+		case 274888:
+			result = "Travis Hodges";
+			break;
+		case 315135:
+			result = "Phero Luong";
+			break;
+		default:
+			result = "Not available";
 		
 		}
+		
+		System.out.println(result);
 	}
 
 }
