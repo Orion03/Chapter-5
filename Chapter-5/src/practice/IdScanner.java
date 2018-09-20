e5e84d54f89db64ac760fd1e18ccd57900855380
JackReis01 <jackreis2019@gmail.com>
2018-09-20 09:04:16 -0500

    updated text

--- Chapter-5/src/practice/IdScanner.java
+++ Chapter-5/src/practice/IdScanner.java
@@ -1,8 +1,25 @@
 package practice;
 
+import java.util.Scanner;
+
 public class IdScanner {
 
 	public static void main(String[] args) {
+		
+		int idNumber;
+		String result;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Scan the barcode to find a person >> ");
+		idNumber = input.nextInt();
+		
+		switch(idNumber)
+		{
+		case 274904:
+			result = "John Brosius";
+			break;
+		}
 	}
 
 }
