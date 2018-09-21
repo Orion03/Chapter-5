419d48a658dc852cc06de878d9f0e03e2a682fb5
JackReis01 <jackreis2019@gmail.com>
2018-09-21 08:59:03 -0500

    updated text

--- Chapter-5/src/gameZone/randomGuess2.java
+++ Chapter-5/src/gameZone/randomGuess2.java
@@ -12,9 +12,8 @@ public class randomGuess2 {
 		
 		String result;
 		
-		computerSecret = 
-		
 		Scanner input = new Scanner(System.in);
+		computerSecret = (int )(Math.random() * 10 + 1);
 		
 		System.out.println("Pick a number, any number as long it's no higher than 10! >> ");
 		userGuess = input.nextInt();
