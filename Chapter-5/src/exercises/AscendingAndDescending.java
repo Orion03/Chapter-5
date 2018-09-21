e40f7841f34e03eae1d3e63610a471418939fbde
JackReis01 <jackreis2019@gmail.com>
2018-09-21 08:40:19 -0500

    updated text

--- Chapter-5/src/exercises/AscendingAndDescending.java
+++ Chapter-5/src/exercises/AscendingAndDescending.java
@@ -45,14 +45,27 @@ public class AscendingAndDescending {
 		{
 			if(num2 <= num3)
 			{
-				
+				result = num2 + " " + num3 + " " + num1;
 			}
 			
 			if (num2 >= num3)
 			{
-				
+				result = num3 + " " + num2 + " " + num1;
 			}
 			
+		}
+		
+		else if (num1 >= num2 && num1 <= num3)
+		{
+			if(num2 <= num3)
+			{
+				result = num2 + " " + num1 + " " + num3;
+			}
+			
+			if(num2 >= num3)
+			{
+				result = num3 + " " + num1 + " " + num2;
+			}
 		}
 		
 		System.out.println(result);
