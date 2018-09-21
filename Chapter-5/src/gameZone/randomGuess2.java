cee0fae3ab51e5ab4641d1fe962f8deb1edf7ba4
JackReis01 <jackreis2019@gmail.com>
2018-09-21 08:50:16 -0500

    updated text

--- Chapter-5/src/gameZone/randomGuess2.java
+++ Chapter-5/src/gameZone/randomGuess2.java
@@ -1,10 +1,19 @@
 package gameZone;
 
+java.util.Scanner;
+
 public class randomGuess2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int userGuess;
+		int computerSecret;
+		
+		String result;
+		
+		Scanner input = new Scanner(System.in);
+		
 	}
 
 }
